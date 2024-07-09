27a11129faf819aeb74dcac6da367c3fb3008f92
Anh Duy <duybtvdu12354@gmail.com>
2024-07-09 11:13:20 +0700

    Update footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -27,66 +27,64 @@ const FooterComponent = () => {
                                     style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                 />
                                 <p>
-                                    Here you can use rows and columns to organize your footer
-                                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                                    elit.
+                                    Đến với Nha Khoa Sức Khỏe để có một hàm răng trắng sáng rạng ngời, một hàm răng khỏe mạnh và chất lượng. Nha Khoa Sức Khỏe, lựa chọn đáng tin cậy cho mọi nhà.
                                 </p>
                             </div>
 
 
 
                             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                <h6 className="text-uppercase fw-bold">Products</h6>
+                                <h6 className="text-uppercase fw-bold">DỊCH VỤ</h6>
                                 <hr
                                     className="mb-4 mt-0 d-inline-block mx-auto"
                                     style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                 />
                                 <p>
-                                    <a href="#!" className="text-dark">MDBootstrap</a>
+                                    <a href="#!" className="text-dark">Làm răng sứ</a>
                                 </p>
                                 <p>
-                                    <a href="#!" className="text-dark">MDWordPress</a>
+                                    <a href="#!" className="text-dark">Niềng răng</a>
                                 </p>
                                 <p>
-                                    <a href="#!" className="text-dark">BrandFlow</a>
+                                    <a href="#!" className="text-dark">Nhổ răng</a>
                                 </p>
                                 <p>
-                                    <a href="#!" className="text-dark">Bootstrap Angular</a>
+                                    <a href="#!" className="text-dark">Vệ sinh răng</a>
                                 </p>
                             </div>
 
                             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
 
-                                <h6 className="text-uppercase fw-bold">Useful links</h6>
+                                <h6 className="text-uppercase fw-bold">HỮU ÍCH</h6>
                                 <hr
                                     className="mb-4 mt-0 d-inline-block mx-auto"
                                     style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                 />
                                 <p>
-                                    <a href="#!" className="text-dark">Your Account</a>
+                                    <a href="#!" className="text-dark">Tài khoản của bạn</a>
                                 </p>
                                 <p>
-                                    <a href="#!" className="text-dark">Become an Affiliate</a>
+                                    <a href="#!" className="text-dark">Lịch làm việc</a>
                                 </p>
                                 <p>
-                                    <a href="#!" className="text-dark">Shipping Rates</a>
+                                    <a href="#!" className="text-dark">Đặt lịch khám</a>
                                 </p>
                                 <p>
-                                    <a href="#!" className="text-dark">Help</a>
+                                    <a href="#!" className="text-dark">Trợ giúp</a>
                                 </p>
                             </div>
 
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
 
-                                <h6 className="text-uppercase fw-bold">Contact</h6>
+                                <h6 className="text-uppercase fw-bold">LIÊN HỆ</h6>
                                 <hr
                                     className="mb-4 mt-0 d-inline-block mx-auto"
                                     style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                 />
-                                <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
-                                <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                                <p><i className="fas fa-home mr-3"></i>200 LÊ VĂN VIỆT, Thành phố Thủ Đức, HCM</p>
+                                <p><i className="fas fa-envelope mr-3"></i> + nhakhoasuckhoe@gmail.com</p>
+                                <p><i className="fas fa-phone mr-3"></i> + 0933 986 097</p>
+                                <p><i className="fas fa-print mr-3"></i> + 0154 576 888</p>
                             </div>
 
                         </div>
