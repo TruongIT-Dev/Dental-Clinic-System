edb0010a92f36fbdae997cf8e1aa37d87fdf7c2e
Anh Duy <duybtvdu12354@gmail.com>
2024-07-12 10:15:45 +0700

    new error page

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,7 +1,15 @@
-const Error = () => {
+import React from 'react';
+import { Button, Result } from 'antd';
+  
+  const Error = () => {
   return (
     <>
-       
+       <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    
+  />
     </>
   )
 }
