bcfa4fb3bbfb7aadc94d94f3c00562406efa114b
Anh Duy <duybtvdu12354@gmail.com>
2024-07-12 10:35:35 +0700

    update footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -61,9 +61,6 @@ const FooterComponent = () => {
                                 <p>
                                     <a href="#!" className="text-dark">Đặt lịch khám</a>
                                 </p>
-                                <p>
-                                    <a href="#!" className="text-dark">Trợ giúp</a>
-                                </p>
                             </div>
 
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
@@ -73,7 +70,7 @@ const FooterComponent = () => {
                                     className="mb-4 mt-0 d-inline-block mx-auto"
                                     style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
                                 />
-                                <p><i className="fas fa-home mr-3"></i> + Cơ sở chính: 200 LÊ VĂN VIỆT, THÀNH PHỐ THỦ ĐỨC, HCM</p>
+                                <p><i className="fas fa-home mr-3"></i> + Cơ sở chính: 200 Lê Văn Việt, Thành Phố Thủ Đức, HCM</p>
                                 <p><i className="fas fa-envelope mr-3"></i> + Email: nhakhoasuckhoe@gmail.com</p>
                                 <p><i className="fas fa-phone mr-3"></i> + 0933 986 097</p>
                                 <p><i className="fas fa-print mr-3"></i> + 0154 576 888</p>
